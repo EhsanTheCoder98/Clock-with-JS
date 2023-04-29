4bae709ddb0aa6e1dda44881926870f121c190d4
Ethan Fathalipour <www.ehsanfathalipour110@gmail.com>
2023-04-29 13:36:02 +0430

    Date Added!

--- code.js
+++ code.js
@@ -3,6 +3,9 @@ function dateAndTime(){
     let h = date.getHours()
     let m = date.getMinutes()
     let s = date.getSeconds()
+    let day = date.getDay()
+    let month = date.getMonth()
+    let year = date.getFullYear()
     let session = "AM"
 
     if(h>12){
@@ -21,10 +24,11 @@ function dateAndTime(){
 
     let time = `${h}:${m}:${s} ${session}`
     document.querySelector(".clock").innerText = time;
+    let dateOfDay = `${year}/${month}/${day}`
+    document.querySelector(".date").innerText = dateOfDay
 }
-
-
-
-
-
 setInterval(dateAndTime,1000)
+
+let date = new Date()
+let fullDate = date.getMonth()
+console.log(fullDate)
